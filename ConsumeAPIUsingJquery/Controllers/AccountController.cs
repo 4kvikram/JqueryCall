13f5e0540c3d862e6822ae84d1fc31245016e877
Vikram <59957554+4kvikram@users.noreply.github.com>
2022-06-13 16:58:10 +0530

    Add files via upload
    added file

---
+++ ConsumeAPIUsingJquery/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumeAPIUsingJquery.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            return View();
+        }      
+    }
+}
---
+++ ConsumeAPIUsingJquery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConsumeAPIUsingJquery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
